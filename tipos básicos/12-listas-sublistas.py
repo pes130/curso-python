2e9a65b5379e16d58fd7e3c960ccf85bcb53e36c
Pablo <pablo.proe.tic@gmail.com>
2020-01-30 13:44:36 +0100

    Añadidos ejemplos de lsitas

---
+++ tipos básicos/12-listas-sublistas.py
@@ -0,0 +1,18 @@
+personajes = ['Ryu', 'Ken', 'Chun-Li', 'Dhalsim', 'Honda']
+#lista[index_inicio:, index_fin]
+print(personajes[1:4]) # ['Ken', 'Chin-Li', 'Dhalsim']
+#['Ryu', 'Ken', 'Chun-Li']
+print(personajes[:3])
+# [, 'Dhalsim', 'Honda']
+print(personajes[3:])
+
+# Pintar el penúltimo
+print(personajes[-2])
+print(personajes[-1])
+print(personajes[-3:])
+
+# Las cadenas de texto se comportan como listas
+nombre = 'Rodrigo'
+print(nombre[1:4])
+print(nombre[-3])
+
